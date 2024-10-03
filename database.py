7b8995ebef6784784c126b2bc569d6e8f182fb38
Carol Fernandes <81500528+CarolFernandes7@users.noreply.github.com>
2024-10-02 21:41:26 -0300

    Update database.py

--- database.py
+++ database.py
@@ -5,23 +5,23 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('data.csv')
 
 # Configurar o tamanho da figura
-fig, ax = plt.subplots(figsize=(10, 2.5))  # Ajuste o tamanho conforme necessário
+fig, ax = plt.subplots(figsize=(10, 2.5))  
 
-# Remover as bordas do gráfico
+
 ax.axis('off')
 
-# Criar uma tabela com os dados do DataFrame
+
 table = ax.table(cellText=data.values, colLabels=data.columns, cellLoc='center', loc='center')
 
-# Ajustar a escala da tabela
-table.scale(1.2, 1.5)  # Aumentar a largura e a altura das células
 
-# Ajustar o layout da tabela
+table.scale(1.2, 1.5)  
+
+
 table.auto_set_font_size(False)
-table.set_fontsize(14)  # Aumentar o tamanho da fonte para melhor visibilidade
+table.set_fontsize(14)  
 
-# Salvar a tabela como uma imagem PNG
+
 plt.savefig('adjusted_database_table.png', bbox_inches='tight', dpi=300)
 
-# Exibir a tabela
+
 plt.show()
